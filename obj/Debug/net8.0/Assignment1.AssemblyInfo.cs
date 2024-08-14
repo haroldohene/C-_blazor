51028864eb53e92d4e97008ce703567f0e9bdcc6
Grld-Andy <ce-hkohene3121@st.umat.edu.gh>
2024-08-14 22:36:04 +0100

    new files

--- obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc8e1e71fc976e1daee0a8e5d33c609dd6e74cb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
